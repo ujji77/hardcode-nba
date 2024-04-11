f5d05dc0b777db785b4ba428872a63b0929fdc54
ujji77 <142092341+ujji77@users.noreply.github.com>
2024-04-11 22:48:40 +0100

    stats box

--- script.js
+++ script.js
@@ -62,7 +62,21 @@ function updatePlayerInfo(playerName) {
             document.getElementById("player-image").src = 'images/' + playerName.toLowerCase() + '.png';
             document.getElementById("player-name").textContent = player.Name;
             document.getElementById("player-meta").textContent = `${player.Team} • ${player.Number} • ${player.Position}`;
+        // Update player stats
+            document.getElementById('mpg').textContent = player.MPG;
+            document.getElementById('gpp').textContent = player.GPP;
+
+            document.getElementById('ppg').textContent = player.PPG;
+            document.getElementById('pgpos').textContent = player.PGpos;
+
+            document.getElementById('apg').textContent = player.APG;
+            document.getElementById('apgpos').textContent = player.APGpos;
+
+            document.getElementById('rpg').textContent = player.RPG;
+            document.getElementById('rpgpos').textContent = player.RPGpos;
         }
+    }).catch(function(error) {
+        console.error("Error updating player info:", error);
     });
 }
 
